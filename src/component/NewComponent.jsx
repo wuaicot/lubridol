6ba2fe30d4c2a64bbd937f4d51a328f3f092ebd3
RODO.L <wuaicot8@gmail.com>
2023-09-09 15:28:06 -0300

    vivo

---
+++ src/component/NewComponent.jsx
@@ -0,0 +1,13 @@
+// NewComponent.jsx
+import React from 'react';
+
+function NewComponent() {
+  return (
+    <div>
+      <h1>This is the New Component</h1>
+      {/* Agrega aquí el contenido de tu nuevo componente */}
+    </div>
+  );
+}
+
+export default NewComponent;
